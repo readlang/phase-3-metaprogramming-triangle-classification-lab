5fd265ffaafc9bfdb8e3d5304aa741662f694adb
Read Langworthy <rlangworthy@gmail.com>
2022-02-16 16:20:20 -0800

    commit tree 3bb9f9f6092a0003141f25b2b24f278cddcfe7d2

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
   # write code here
+
+  attr_accessor :s1, :s2, :s3
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  class TriangleError < StandardError
+    puts "ERROR!!!"
+  end
+
+  def kind
+    if self.s1 == 0 || self.s2 == 0 || self.s3 == 0
+      raise TriangleError
+    end
+
+    if self.s1 + self.s2 <= self.s3 || self.s1 + self.s3 <= self.s2 || self.s2 + self.s3 <= self.s1
+      raise TriangleError
+    end
+
+
+   if self.s1 == self.s2 && self.s1 == self.s3
+     return :equilateral
+   elsif self.s1 == self.s2 || self.s1 == self.s3 || self.s2 == self.s3
+     return :isosceles
+   else
+     return :scalene
+   end
+  end
+
+  
+
+  
+
 end
